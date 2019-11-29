e2f5deeb56988e882ee3d3f0a97fcd4b38006613
Andrew <more.andrew@yandex.ru>
2019-11-29 12:43:14 -0500

    Update
    Edited code accordingly to PEP 8 for better readability

--- parseYM.py
+++ parseYM.py
@@ -1,9 +1,10 @@
 import requests
 import json
 
-
 ym_token="yourtoken" # see the details about getting an access token in README 
 ids="your counter id or ids"
+
+
 def YMquery(startDate, endDate, searchEngine, startURL, token):
     adr="https://api-metrika.yandex.ru/stat/v1/data?ids="+ids+"&metrics=ym:s:visits,ym:s:bounceRate&startDate="\
         +startDate\
@@ -20,8 +21,10 @@ def YMquery(startDate, endDate, searchEngine, startURL, token):
     parsed = json.loads(r.text)
 
     return parsed
+    
+    
     # Here's the page types I want to gather data for 
-pageTypes = [
+page_types = [
     ["All", "\'https://myexamplestore.com/\'"],
     ["Main Page", "\'https://myexamplestore.com/$\'"],
     ["All Mac Pages", "\'https://myexamplestore.com/brand/apple/macs/\'"],
@@ -29,26 +32,29 @@ pageTypes = [
     ["Just some page", "\'https://myexamplestore.com/sample_page/$\'"]
 ]
 
-def main():
 
+def main():
     # Set the parameters
-    startDate = "2016-09-01"
-    endDate = "2016-09-30"
-    searchEngine = "\'yandex\'" # or "\'google\'" for Google etc.
+    start_date = "2016-09-01"
+    end_date = "2016-09-30"
+    search_engine = "\'yandex\'" # or "\'google\'" for Google etc.
+    
     # Let's write the results into a file
     f = open('output.txt', 'w')
-    f.write("startDate: "+startDate+'\n'+"endDate: "+endDate+'\n'+'\n')
+    f.write("startDate: " + start_date + '\n'+"endDate: " + end_date+'\n'+'\n')
     f.write("-----Yandex-----"+'\n')
     i=0
-    for counter in pageTypes:
-        parsed = YMquery(startDate, endDate, searchEngine, counter[1], ym_token)
+    for counter in page_types:
+        parsed = YMquery(start_date, end_date, search_engine, counter[1], ym_token)
         visits = parsed['data'][0]['metrics'][0]
-        bounceRate = parsed['data'][0]['metrics'][1]
-        brFormatted = round(bounceRate, 2)/100
-        result = counter[0]+'\t'+str(round(visits))+'\t'+str(brFormatted)
-        f.write(result+'\n')
-        i=i+1
-        print(str(i)+" of "+str(pageTypes.__len__())+" parsed (Y)")
+        bounce_rate = parsed['data'][0]['metrics'][1]
+        
+        bounce_rate_formatted = round(bounce_rate, 2)/100
+        result = counter[0] + '\t' + str(round(visits)) + '\t' + str(bounce_rate_formatted)
+        
+        f.write(result + '\n')
+        i = i + 1
+        print(str(i) + " of " + str(page_types.__len__()) + " parsed (Y)")
 
     f.close()
 
